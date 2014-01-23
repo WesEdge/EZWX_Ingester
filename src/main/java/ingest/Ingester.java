5eeea9ad273b95a3a39dda071b89eeed64b23f8a
HoboShredder <wes.edge@weather.com>
2014-01-23 00:03:18 -0600

    Persister now receives a queue message & grabs the grib file from memcached.

--- src/main/java/ingest/Ingester.java
+++ src/main/java/ingest/Ingester.java
@@ -10,6 +10,8 @@ import EZWX.mq.MQArgs;
 import EZWX.core.Downloader;
 import net.rubyeye.xmemcached.MemcachedClient;
 import net.rubyeye.xmemcached.XMemcachedClient;
+import net.rubyeye.xmemcached.transcoders.SerializingTranscoder;
+import ucar.grib.GribChecker;
 
 public class Ingester implements MQActor  {
 
@@ -27,9 +29,13 @@ public class Ingester implements MQActor  {
         // save to memcached
         String memCachedHost = Properties.getValue("memCachedHost");
         int memCachedPort = Integer.parseInt(Properties.getValue("memCachedPort"));
+        int memCachedMaxObjectSizeMB = Integer.parseInt(Properties.getValue("memCachedMaxObjectSizeMB"));
+        int memCachedCacheDurationMinutes = Integer.parseInt(Properties.getValue("memCachedCacheDurationMinutes"));
         String memCachedKey = Properties.getValue("memCachedKey");
         MemcachedClient client = new XMemcachedClient(memCachedHost, memCachedPort);
-        client.set(memCachedKey, 3600, bytes); // cache for 1 hour
+        //set maximum to 10MB
+        client.setTranscoder(new SerializingTranscoder(memCachedMaxObjectSizeMB*1024*1024));
+        client.set(memCachedKey, memCachedCacheDurationMinutes * (3600/60), bytes); // cache for 1 hour
 
         //broadcast the event
         (new Messenger()).send();
